40779bb433660c74647353efeb37813742b20822
Chris <Chris@Chris_Acer.Speedport_W_724V_09011603_00_012>
2015-10-22 17:14:12 +0200

    bla bla

--- TimetableApp/src/HelloWorld.java
+++ TimetableApp/src/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		Vorlesungsliste liste = new Vorlesungsliste();
 		liste = new TimeTableSql().SelectVorlesungen();
-		System.out.println(liste.getWeekdayvorlesungen(Calendar.FRIDAY));
+		System.out.println(liste.getWeekdayvorlesungen(Calendar.WEDNESDAY));
 	}
 }
